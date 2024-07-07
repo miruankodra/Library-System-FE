6178a92bcd1ac05f69ff4def29a62276d755f1bf
miruan kodra <m.kodra@bluehat.al>
2024-07-07 02:11:49 +0200

    add inputs

--- src/app/pages/authentication/login/login.component.ts
+++ src/app/pages/authentication/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {LibInputComponent} from "../../../components/shared/lib-input/lib-input.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    LibInputComponent
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
